8c017735a63adaa25bce698f58a1b17407a848e9
Victor <victor.amado@blautech.us>
2022-08-18 12:43:51 -0600

    agregar modulo usuarios a backend

--- backend/src/app/routes/usuario.js
+++ backend/src/app/routes/usuario.js
@@ -1,33 +1,68 @@
 const conn = require('../../config/database');
 
 module.exports = (app) => {
+
+    // Get de todos los usuarios
     app.get("/usuario", (req, res) => {
-        let query = "SELECT id, nombre, usuario, password, fecha_nacimiento, sexo FROM usuario";
+        let query = "SELECT idUsuario, nombre, departamento, idTipoUsuario, usuario, contraseña FROM usuarios";
         conn.query(query, (err, rows, field) => {
-            if (err) res.status(400).json({status: 0, message: "No se pudo obtener informacion"});
-            else res.json({status: 1, usuarios : rows});
+            if (err) res.status(400).json({ status: 0, message: "No se pudo obtener informacion" });
+            else res.json({ status: 1, usuarios: rows });
         })
     });
 
-    app.get("/usuario/usuario/:usuario/:password", (req, res) => {
-        let query = `SELECT id, nombre, usuario, password, fecha_nacimiento, sexo FROM usuario where usuario = '${req.params.usuario}' and password = '${req.params.password}'`;
+    // Get de un usuario en particular
+    app.get("/usuario/:usuario/:password", (req, res) => {
+        let query = `SELECT idUsuario, nombre, departamento, idTipoUsuario, usuario, contraseña ` +
+            `FROM usuarios where usuario = '${req.params.usuario}' ` +
+            `and contraseña = '${req.params.password}'`;
         conn.query(query, (err, rows, field) => {
-            if (err) res.status(400).json({status: 0, message: "No se pudo obtener informacion"});
-            else res.json({status: 1, usuarios : rows});
+            if (err) res.status(400).json({ status: 0, message: "No se pudo obtener informacion" });
+            else res.json({ status: 1, usuarios: rows });
         })
     });
-    
+
+    // Post para crear un usuario
     app.post('/usuario', (req, res, next) => {
-        let query = `INSERT INTO usuario (nombre, usuario, password, fecha_nacimiento, sexo) VALUES ('${req.body.nombre}', '${req.body.usuario}', '${req.body.password}', '${req.body.fecha_nacimiento}', '${req.body.sexo}')`;
+        let query = `INSERT INTO usuarios (nombre, departamento, idTipoUsuario, usuario, contraseña) ` +
+            `VALUES ('${req.body.nombre}', '${req.body.departamento}', 2, ` +
+            `'${req.body.usuario}', '${req.body.contraseña}')`;
         conn.query(query, (err, rows, cols) => {
             if (err) {
-                res.json({status: 0, message: "Error en la db"}); 
+                res.json({ status: 0, message: "Error en la db" });
             } else {
-                res.json({status: 1, message: "Usuario insertado satisfactoriamente"});
+                res.json({ status: 1, message: "Usuario insertado satisfactoriamente" });
             }
         });
     });
 
-    
-}
+    // Put para modificar un usuario
+    app.put('/usuario/:idUsuario', (req, res) => {
+        let query = `UPDATE usuarios SET nombre = '${req.body.nombre}', ` +
+            `departamento = '${req.body.departamento}', ` +
+            `idTipoUsuario = '${req.body.idTipoUsuario}', ` +
+            `usuario = '${req.body.usuario}', ` +
+            `contraseña = '${req.body.contraseña}' ` +
+            `where idUsuario = '${req.params.idUsuario}'`;
+        conn.query(query, (err, rows, cols) => {
+            if (err) {
+                res.json({ status: 0, message: "Error en la db" });
+            } else {
+                res.json({ status: 1, message: "Usuario modificado satisfactoriamente" });
+            }
+        });
+    });
 
+    // Delete para eliminar un usuario
+    app.delete('/usuario/:idUsuario', (req, res) => {
+        let query = `DELETE FROM usuarios WHERE idUsuario = '${req.params.idUsuario}'`;
+        conn.query(query, (err, rows, cols) => {
+            if (err) {
+                res.json({ status: 0, message: "Error en la db" });
+            } else {
+                res.json({ status: 1, message: "Usuario eliminado satisfactoriamente" });
+            }
+        });
+    });
+
+}
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const conn = mysql.createConnection({
     host: '127.0.0.1',
+    port: '3306',
     user: 'root',
     password: 'root',
-    database: 'faw'
+    database: 'mineprocess',
+    insecureAuth: true
 });
 
 conn.connect(err => {
